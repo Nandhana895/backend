05fc333a311e95458d780c107989d1814a6988dd
Nandhana895 <nandhana895@gmail.com>
2025-04-10 09:38:36 +0530

    editted

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ app.use(express.json())
 const cors=require('cors')
 app.use(cors())
 const todo=require('./Routes/Todolistroutes')
-port=3005
+port=3003
 const mongoose = require('mongoose')
 mongoose.connect('mongodb+srv://nandhanasunil2026:Nandhana123@cluster0.tdv63.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0').then(()=>
 {
@@ -12,6 +12,7 @@ mongoose.connect('mongodb+srv://nandhanasunil2026:Nandhana123@cluster0.tdv63.mon
 })
 app.use(todo)
 app.listen(port)
+
 
 
  
